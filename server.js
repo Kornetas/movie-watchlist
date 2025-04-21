608c06d0b792233e7dbbddffac623939c35e5949
Arek <arek93r@gmail.com>
2025-04-21 21:38:28 +0200

    create public folder and serve index.html with express

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+
+
+app.use(express.static('public'))
+
 //this route just say hello for test if server is alive
-app.get("/", (req, res) => {
-  res.send("Cześć nowa appka");
-});
+// app.get("/", (req, res) => {
+//   res.send("Cześć nowa appka");
+// });
 
 // init server
 app.listen(PORT, () => {
