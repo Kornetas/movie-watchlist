059ca4927ef425ebe9229e7ac11a64026ea9a97e
Arek <arek93r@gmail.com>
2025-04-21 21:12:20 +0200

    server is alive! just saying hello for now

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//this route just say hello for test if server is alive
+app.get("/", (req, res) => {
+  res.send("Cześć nowa appka");
+});
+
+// init server
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
